98e3939c77c4e19cf5eb3c65f9772ae748c0c605
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-10-02 14:53:50 +0100

    Added code generation templates for labels, let expressions, begin statements and parenthesis

--- src/GPC/CodeGen.hs
+++ src/GPC/CodeGen.hs
@@ -6,8 +6,32 @@ import GPC.AST
 
 -- Generate GPIR code from Program AST
 genCode :: Program -> String
-genCode (Program xs) = concatMap (genTopLevel) xs
+genCode (Program xs) = concatMap genTopLevel xs
+
 
 -- Generate code from Top Level statements
 genTopLevel :: TopLevel -> String
-genTopLevel tl = "placeholder"
+genTopLevel (TlStmt ss) = genStmt ss
+genTopLevel (Func _ n args rest) = case args of
+    -- Simple begin label for no argument functions
+    [] -> letExp $ label n $ begin $ concatMap genStmt rest
+    -- Need to generate lambda for functions with arguments
+    _  -> "placeholder"
+
+-- Generate code for statements
+genStmt :: Stmt -> String
+genStmt s = "placeholder"
+
+
+label :: String -> String -> String
+label n s = "'" ++ (parens $ "label " ++ s)
+
+letExp :: String -> String
+letExp s =  parens $ "let " ++ s 
+
+begin :: String -> String
+begin s = parens $ "begin " ++ s
+
+
+parens :: String -> String
+parens s = "(" ++ s ++ ")" 
