d104b1c2cd32a4e3b90e3805c395085c31345c02
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-11-25 15:40:41 +0000

    Added AST for GPIR interemediate code

---
+++ src/GPC/GPIRAST.hs
@@ -0,0 +1,29 @@
+-- AST for GPIR
+
+module GPC.GPIRAST (
+    SymbolTree(..),
+    GannetSymbol(..),
+    OpSymbol(..),
+    Quoted
+ )
+where
+
+data SymbolTree = Symbol GannetSymbol | SymbolList (Quoted,[SymbolTree])
+
+-- (label LABEL ( ... ))    
+data GannetSymbol = 
+      ConstSymbol (Quoted, String) 
+    | GOpSymbol OpSymbol 
+    | LabelKeywordSymbol (Quoted, String)
+    | LabelSymbol (Quoted, String)
+
+
+data OpSymbol = MkOpSymbol {
+                        quoted  :: Quoted, -- ^ Whether symbol is quoted or not
+                        node    :: (String, Int), -- ^ Node to run on
+                        lib     :: String, -- ^ C++ library
+                        gClass   :: String, -- ^ C++ class name
+                        method  :: String  -- ^ C++ method call name
+                    } deriving (Eq,Ord)
+
+type Quoted = Bool
