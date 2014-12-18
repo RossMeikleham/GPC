6392a5857534d5754945c3a714ef1b5b6b08a93b
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-12-18 14:05:18 +0000

    Created placeholders for entry arguments

--- src/GPC/GenGPIR.hs
+++ src/GPC/GenGPIR.hs
@@ -4,32 +4,32 @@
 
 module GPC.GenGPIR (genGPIR) where
 
-import Control.Lens
-import Control.Applicative hiding ((<|>), many, optional, empty)
-import Control.Monad.State.Lazy
-import Control.Error.Util
-import qualified Data.Map as M
-import GPC.AST
-import GPC.GPIRAST
+import           Control.Applicative      hiding (empty, many, optional, (<|>))
+import           Control.Error.Util
+import           Control.Lens
+import           Control.Monad.State.Lazy
+import qualified Data.Map                 as M
+import           GPC.AST
+import           GPC.GPIRAST
 
 type ConstVarTable = M.Map Ident Literal
 type FunTable = M.Map Ident ([Ident], BlockStmt)
 type VarRegTable = M.Map Ident Integer
 
 data CodeGen = CodeGen {
-   _funTable :: FunTable,  -- ^ Store symbol tree for functions
-   _constTable :: ConstVarTable,
-   _varId :: Integer, -- ^ Current variable id for mapping to registers 
+   _funTable    :: FunTable,  -- ^ Store symbol tree for functions
+   _constTable  :: ConstVarTable,
+   _varId       :: Integer, -- ^ Current variable id for mapping to registers
    _varRegTable :: VarRegTable -- ^ maps variable identifier
 }
 
 -- Create lenses to access Block fields easier
 makeLenses ''CodeGen
 
-type GenState a = StateT CodeGen (Either String) a 
+type GenState a = StateT CodeGen (Either String) a
 
 isAssign :: TopLevel -> Bool
-isAssign tl = case tl of 
+isAssign tl = case tl of
     TLAssign _ -> True
     _ -> False
 
@@ -51,7 +51,7 @@ isNonEntryFunc eName tl = case tl of
 -- | Update the register table and counter
 -- | return the next available register
 updateRegs :: Ident -> GenState Ident
-updateRegs ident = do  
+updateRegs ident = do
     vId <- use varId
     vRegTable <- use varRegTable
     let newVarId = vId + 1
@@ -60,7 +60,7 @@ updateRegs ident = do
     return $ Ident (show newVarId)
 
 genGPIR :: String -> Program -> Either String SymbolTree
-genGPIR name (Program tls) = case runStateT (genTopLevel name tls) initial of 
+genGPIR name (Program tls) = case runStateT (genTopLevel name tls) initial of
     Left s -> Left s
     (Right (tl, _)) -> Right tl
  where initial = CodeGen M.empty M.empty 0 M.empty
@@ -68,17 +68,14 @@ genGPIR name (Program tls) = case runStateT (genTopLevel name tls) initial of
 
 genTopLevel :: String -> [TopLevel] -> GenState SymbolTree
 genTopLevel name tls = do
-    genTLAssigns tls 
+    genTLAssigns tls
     genFuncs tls
     let tls' = filter (\x -> not (isAssign x || isObject x || isNonEntryFunc name x)) tls
     symbolTrees <- mapM genTopLevelStmt tls'
-    return $ SymbolList False $  
-      if length symbolTrees > 1 
-         then seqSymbol : symbolTrees
-         else symbolTrees
- where 
+    return $ SymbolList False $ seqSymbol : symbolTrees
+ where
     seqSymbol = Symbol $ GOpSymbol $
-                MkOpSymbol False ("", 0) ["CoreServices", "Seq", "seq"]
+                MkOpSymbol False ("", 0) ["CoreServices", "Begin", "begin"]
 
 
 -- | Generate all Top Level Assignments
@@ -87,23 +84,23 @@ genTLAssigns tls = mapM_ genTLAssign $ filter isAssign tls
 
 genTLAssign :: TopLevel -> GenState ()
 genTLAssign (TLAssign (Assign _ ident expr)) = case expr of
-    (ExpLit l) -> do         
+    (ExpLit l) -> do
         cTable <- use constTable
-        assign constTable $ M.insert ident l cTable 
-    (ExpPointer _) -> return () 
-    _ -> lift $ Left $ show expr --"Compiler error, in top level assignment code generation"  
+        assign constTable $ M.insert ident l cTable
+    (ExpPointer _) -> return ()
+    _ -> lift $ Left $ show expr --"Compiler error, in top level assignment code generation"
 genTLAssign _ = lift $ Left "Not top level Assignment statement"
 
 
 -- | Store all functions, to be evaluated later
 genFuncs :: [TopLevel] -> GenState ()
-genFuncs tls = mapM_ genFunc $ filter isFunc tls  
+genFuncs tls = mapM_ genFunc $ filter isFunc tls
 
 genFunc :: TopLevel -> GenState ()
 genFunc (Func _ name args stmts) = do
     fTable <- use funTable
-    assign funTable $ M.insert name (map snd args, stmts) fTable 
-         
+    assign funTable $ M.insert name (map snd args, stmts) fTable
+
 genFunc a = lift $ Left $ "Not Function definition" ++ show a
 
 
@@ -111,21 +108,21 @@ genFunc a = lift $ Left $ "Not Function definition" ++ show a
 genTopLevelStmt :: TopLevel -> GenState SymbolTree
 genTopLevelStmt tl = case tl of
       (TLConstructObjs cObjs) -> genTLConstructObjs cObjs
-      (Func _ _ _ bStmt) -> genEntryFunc bStmt
-      _ -> lift $ Left $ "Compiler error, shouldn't contain Top Level " ++ 
+      (Func _ _ args bStmt) -> genEntryFunc bStmt args
+      _ -> lift $ Left $ "Compiler error, shouldn't contain Top Level " ++
                 "Assignments, Object Decls or non Entry functions"
 
 
-         
+
 -- | Generate Object Constructors
 genTLConstructObjs :: ConstructObjs -> GenState SymbolTree
-genTLConstructObjs (ConstructObjs ns var exprs) = do 
-    let constructor = Symbol $ GOpSymbol $ 
-                    MkOpSymbol False ("", 0) (map show ns) 
+genTLConstructObjs (ConstructObjs ns var exprs) = do
+    let constructor = Symbol $ GOpSymbol $
+                    MkOpSymbol False ("", 0) (map show ns)
     args <- mapM checkConst exprs
     let args' = map (Symbol . ConstSymbol True . show) args
 
-    case var of  
+    case var of
         -- TODO work out how to map
         (VarArrayElem _ indexExpr) -> void $ checkConst indexExpr
         (VarIdent _) -> return ()
@@ -134,15 +131,17 @@ genTLConstructObjs (ConstructObjs ns var exprs) = do
 
 
 -- | Generate Program
-genEntryFunc :: BlockStmt -> GenState SymbolTree
-genEntryFunc (BlockStmt stmts) = SymbolList True <$> mapM genStmt stmts
+genEntryFunc :: BlockStmt -> [(Type, Ident)] ->  GenState SymbolTree
+genEntryFunc (BlockStmt stmts) args = do
+    mapM_(updateRegs) (map snd args) -- Create placeholder for entry args 
+    SymbolList True <$> mapM genStmt stmts
 
 genStmt :: Stmt -> GenState SymbolTree
 genStmt stmt = case stmt of
 
     -- When assigning a variable need to write it to a register
     AssignStmt (Assign _ name expr) -> do
-        let assignSymbol = Symbol $ GOpSymbol $ 
+        let assignSymbol = Symbol $ GOpSymbol $
                             MkOpSymbol False ("", 0) ["CoreServices", "reg", "write"]
         expr' <- genExpr expr
         reg <- updateRegs name
@@ -165,7 +164,7 @@ genStmt stmt = case stmt of
         return $ SymbolList False stmts'
 
     MethodStmt (MethodCall cName mName exprs) -> do
-        let call = Symbol $ GOpSymbol $ 
+        let call = Symbol $ GOpSymbol $
                     MkOpSymbol False ("Dummy", 0) ["temp", show cName, show mName]
         exprs' <- mapM genExpr exprs
         return $ SymbolList False (call : exprs')
@@ -177,22 +176,22 @@ genStmt stmt = case stmt of
         stmt'' <- genStmt stmt'
         let dummyStmt = Symbol $ ConstSymbol True "0"
         return $ SymbolList False [ifSymbol, cond, stmt'', dummyStmt]
-        
-    IfElse expr stmt1 stmt2 -> do                
+
+    IfElse expr stmt1 stmt2 -> do
         let ifSymbol = Symbol $ GOpSymbol $
                         MkOpSymbol False ("Dummy", 0) ["CoreServices", "IF", "if"]
         cond <- genExpr expr
         stmt1' <- genStmt stmt1
         stmt2' <- genStmt stmt2
         return $ SymbolList False [ifSymbol, cond, stmt1', stmt2']
-       
+
     Return expr -> do
         let returnSymbol = Symbol $ GOpSymbol $
                             MkOpSymbol False ("Dummy", 0) ["CoreServices", "RETURN", "return"]
         expr' <- genExpr expr
         return $ SymbolList False [returnSymbol, expr']
-        
-    -- For now fully loop unroll 
+
+    -- For now fully loop unroll
     ForLoop ident start stop step stmt' -> do
        start' <- getInt start
        stop'  <- getInt stop
@@ -200,21 +199,21 @@ genStmt stmt = case stmt of
        if start' > stop' then lift $ Left "For loop error, start can't be greater than stop"
        else if step' == 0 || step' < 0 then lift $ Left "For loop error, infinite loop generated"
        else do
-           let unrolledStmts = map (\i -> 
-                                genInlineStmt [(ident, ExpLit (Number (Left i)))] (BStmt stmt'))  
+           let unrolledStmts = map (\i ->
+                                genInlineStmt [(ident, ExpLit (Number (Left i)))] (BStmt stmt'))
                                     [start', start' + step' .. stop' - 1]
            unrolledStmts' <- mapM genStmt unrolledStmts
            return $ SymbolList False unrolledStmts'
 
-         
- where 
+
+ where
     getInt :: Expr -> GenState Integer
     getInt (ExpLit (Number (Left i))) = return i
     getInt _ = lift $ Left "Compiler error, expected integer value from expression"
 
 genExpr :: Expr -> GenState SymbolTree
 genExpr expr = case expr of
-    
+
     ExpBinOp bOp lExpr rExpr -> do
         let method = case bOp of
                     Add -> "plus"
@@ -237,7 +236,7 @@ genExpr expr = case expr of
                     BOr -> "bor"
 
         let binSymbol = Symbol $ GOpSymbol $
-                        MkOpSymbol False ("Dummy", 0) ["CoreServices", "ALU", method] 
+                        MkOpSymbol False ("Dummy", 0) ["CoreServices", "ALU", method]
 
         lExpr' <- genExpr lExpr
         rExpr' <- genExpr rExpr
@@ -260,41 +259,41 @@ genExpr expr = case expr of
         return $ SymbolList False stmts'
 
     ExpMethodCall (MethodCall cName mName exprs) -> do
-        let call = Symbol $ GOpSymbol $ 
+        let call = Symbol $ GOpSymbol $
                     MkOpSymbol False ("Dummy", 0) ["temp", show cName, show mName]
         exprs' <- mapM genExpr exprs
         return $ SymbolList False (call : exprs')
 
     -- Encounterd a variable, need to read it from its register
-    ExpIdent ident -> do 
+    ExpIdent ident -> do
         regTable <- use varRegTable
         reg <- lift $ note regNotFound $ M.lookup ident regTable
-        let regSymbol = Symbol $ GOpSymbol $ 
+        let regSymbol = Symbol $ GOpSymbol $
                         MkOpSymbol False ("", 0) ["CoreServices", "Reg", "read"]
         return $ SymbolList False  [regSymbol, Symbol $ ConstSymbol True (show reg)]
      where regNotFound = "Compiler error, register for ident " ++ show ident ++ "not found"
 
     ExpLit lit -> return $ Symbol $ ConstSymbol True (show lit)
-    
+
     ExpPointer _ -> return EmptyTree
 
 -- | Generate Inline Function by replacing all identifieres
 -- | in scope with supplied argument expressions
 genInlineFunc :: Ident -> [Expr] -> GenState BlockStmt
 genInlineFunc name args = do
-    fTable <- use funTable 
-    case M.lookup name fTable of 
+    fTable <- use funTable
+    case M.lookup name fTable of
         Just (idents, stmts) -> do
             let (BStmt stmts') = genInlineStmt (zip idents args) (BStmt stmts)
             return stmts'
-            
+
         Nothing -> lift $ Left "Compiler error genInlineFunc"
 
 
 genInlineStmt :: [(Ident, Expr)] -> Stmt -> Stmt
 genInlineStmt exprs stmt = case stmt of
 
-    AssignStmt (Assign gType name expr) -> 
+    AssignStmt (Assign gType name expr) ->
         AssignStmt (Assign gType name $ replaceExprIdents exprs expr)
 
     FunCallStmt (FunCall name args) ->
@@ -305,38 +304,38 @@ genInlineStmt exprs stmt = case stmt of
 
     If expr stmt' ->
         If (replaceExprIdents exprs expr) (genInlineStmt exprs stmt')
-        
-    IfElse expr stmt1 stmt2 -> 
-        IfElse (replaceExprIdents exprs expr) (genInlineStmt exprs stmt1) 
+
+    IfElse expr stmt1 stmt2 ->
+        IfElse (replaceExprIdents exprs expr) (genInlineStmt exprs stmt1)
                                               (genInlineStmt exprs stmt2)
 
     Return expr -> Return $ replaceExprIdents exprs expr
 
-    ForLoop name start stop step (BlockStmt stmts) -> 
+    ForLoop name start stop step (BlockStmt stmts) ->
         ForLoop name (replaceExprIdents exprs start)
                      (replaceExprIdents exprs stop)
                      (replaceExprIdents exprs step)
-                     (BlockStmt $ genBlock stmts) 
+                     (BlockStmt $ genBlock stmts)
 
 
     Seq (BlockStmt stmts) -> Seq $ BlockStmt $ genBlock stmts
 
     BStmt (BlockStmt stmts) -> BStmt $ BlockStmt $ genBlock stmts
 
-  where 
+  where
     -- Once the identifier goes out of scope we don't replace it
     -- map up until the identifier is out of scope, and then add on the rest of
-    -- the unmapped statements to the block as they won't need any substitution 
+    -- the unmapped statements to the block as they won't need any substitution
     genBlock :: [Stmt] -> [Stmt]
     genBlock stmts = mappedVals ++ drop (length mappedVals) stmts
       where mappedVals = incMapWhile inScope (genInlineStmt exprs) stmts
     inScope (AssignStmt (Assign _ name _)) = name `notElem` idents
-    inScope _ = True 
+    inScope _ = True
     idents = map fst exprs
 
 
 replaceExprIdents :: [(Ident, Expr)] -> Expr -> Expr
-replaceExprIdents replaceExprs givenExpr = 
+replaceExprIdents replaceExprs givenExpr =
     foldl (\gExpr (ident, rExpr) -> replaceExprIdent ident rExpr gExpr) givenExpr replaceExprs
 
 -- | Replace all instances of an identity in an expression
@@ -344,24 +343,24 @@ replaceExprIdents replaceExprs givenExpr =
 replaceExprIdent :: Ident -> Expr -> Expr -> Expr
 replaceExprIdent ident replaceExpr givenExpr = case givenExpr of
 
-    ExpBinOp binOp lExpr rExpr -> 
+    ExpBinOp binOp lExpr rExpr ->
         ExpBinOp binOp (replaceExprIdent ident replaceExpr lExpr)
                        (replaceExprIdent ident replaceExpr rExpr)
 
     ExpUnaryOp unOp expr -> ExpUnaryOp unOp (replaceExprIdent ident replaceExpr expr)
 
     ExpFunCall (FunCall name exprs) ->
-        ExpFunCall (FunCall name $ 
+        ExpFunCall (FunCall name $
             map (replaceExprIdent ident replaceExpr) exprs)
 
     ExpMethodCall (MethodCall cName mName exprs) ->
-        ExpMethodCall (MethodCall cName mName $ 
+        ExpMethodCall (MethodCall cName mName $
             map (replaceExprIdent ident replaceExpr) exprs)
 
     ExpIdent expId -> if expId == ident then replaceExpr else ExpIdent expId
 
     ExpLit lit -> ExpLit lit
-    
+
     ExpPointer p -> ExpPointer p
 
 -- | inclusive MapWhile function, returns results which satisfy a condition
@@ -373,6 +372,6 @@ incMapWhile _ _ [] = []
 
 
 checkConst :: Expr -> GenState Literal
-checkConst expr = case expr of 
+checkConst expr = case expr of
     (ExpLit l) -> return l
     _ -> lift $ Left "Expected constant expression"
