db5e311e12d7598a692049e3fb33093651e967ce
RossMeikleham <rossmeikleham@hotmail.co.uk>
2015-01-07 15:06:03 +0000

    Added source file tests which can be placed in the examples folder, allows easy/quick testing of language

---
+++ tests/GPC/CompileTests.hs
@@ -0,0 +1,73 @@
+-- Attempt to run the entire compiler on
+-- source files, check files in the pass file
+-- successfully compile, and that fails in the
+-- fail folder, fail to compile at some point
+
+module GPC.CompileTests (compileTests) where
+
+import qualified Test.Framework.Providers.API as TFA
+import Test.Framework.Providers.HUnit
+import Test.HUnit
+import System.Directory
+
+import Data.List
+
+import GPC.Parser
+import GPC.TypeScopeChecker
+import GPC.GenGPIR
+
+passDir =  "examples/pass/"
+failDir =  "examples/fail/"
+
+sourceFiles :: String -> IO [String]
+sourceFiles dir = do 
+    files <- getDirectoryContents dir
+    return $ filter (isSuffixOf ".gpc") files
+
+
+passTests :: IO TFA.Test
+passTests = do 
+    filePaths <- sourceFiles passDir
+    let tests = map passTest filePaths
+    return $ TFA.testGroup "Pass Tests" tests
+
+passTest :: String -> TFA.Test
+passTest file = testCase file ( do
+    source <- readFile filePath
+    case parseSource source of
+        Left err -> assertFailure err
+        Right v ->  do 
+            case runTypeChecker v of
+                Left err -> assertFailure err
+                Right reduced -> case genGPIR fileName reduced of
+                    Left err -> assertFailure err
+                    Right _ -> return ()
+    )
+ where
+    filePath = passDir ++ file 
+    fileName = reverse $ drop 4 $ reverse file
+
+failTests :: IO TFA.Test
+failTests = do 
+    filePaths <- sourceFiles failDir
+    let tests = map failTest filePaths
+    return $ TFA.testGroup "Fail Tests" tests
+
+failTest :: String -> TFA.Test
+failTest file = testCase file ( do
+    source <- readFile filePath
+    case parseSource source of
+        Left _ -> return () 
+        Right v ->  do 
+            case runTypeChecker v of
+                Left _ -> return ()
+                Right reduced -> case genGPIR fileName reduced of
+                    Left _ -> return ()
+                    Right _ -> assertFailure "Program shouldn't have compiled"
+    )
+ where
+    filePath = failDir ++ file 
+    fileName = reverse $ drop 4 $ reverse file
+
+compileTests =  
+    TFA.testGroup "Compiler Tests" $ map TFA.buildTest [passTests, failTests]
--- tests/GPC/Main.hs
+++ tests/GPC/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import GPC.ParserTests
 import GPC.TypeTests
 import GPC.CodeGenTests
+import GPC.CompileTests
 import Test.Framework.Runners.Console (defaultMain)
 
-main = defaultMain $ [parserTests, typeTests, codeGenTests]
+main = defaultMain $ [parserTests, typeTests, codeGenTests, compileTests]
